27619b79f50e155f2a366d3789443b893545d5a5
quiesscent <ciphedc@gmail.com>
2024-10-03 11:22:41 +0300

    landing page and front page design

--- main/views.py
+++ main/views.py
@@ -3,4 +3,10 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 def index(request):
-	return render(request, 'main.html')
+	return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
