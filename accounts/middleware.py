d7184db5ac86135bb5b3b8e22002d88f1a18a6ae
quiesscent <ciphedc@gmail.com>
2024-10-09 14:39:53 +0300

    Member Details Display View

---
+++ accounts/middleware.py
@@ -0,0 +1,11 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.shortcuts import redirect
+from django.contrib import messages
+
+class CheckSuperuserMiddleware(MiddlewareMixin):
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        # Check if the user is not a superuser and accessing a restricted view
+        if not request.user.is_superuser and request.path == '/admin/':
+            messages.error(request, "You do not have permission to access this page login as admin.")
+            return redirect('/dashboard')
+        return None
--- accounts/modules.py
+++ accounts/modules.py
@@ -9,5 +9,5 @@ def generate_unique_membership_number():
     characters = string.ascii_uppercase + string.digits
     while True:
         membership_number = ''.join(random.choices(characters, k=6))
-        if not CustomUser.objects.filter(membership_no=membership_number).exists():
+        if not CustomUser.objects.filter(member_no=membership_number).exists():
             return membership_number
