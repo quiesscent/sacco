cde46019b513191041e507fec5cf7884aad37839
quiesscent <ephesianslewis@gmail.com>
2025-06-07 22:30:29 +0300

    minors

--- sacco/settings.py
+++ sacco/settings.py
@@ -19,8 +19,6 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-0ip+zlq*qf&zt8n3x$9mp-kc-etpyjo6mynvcg6j44e5%h2=ny'
